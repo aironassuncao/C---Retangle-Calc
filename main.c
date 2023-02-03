332858755741e2455b3cce389d68c4d49d6cc6d8
Airon Assunção <124368171+aironassuncao@users.noreply.github.com>
2023-02-03 13:28:33 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  double base, altura, diagonal, perimetro, area;
+
+  printf ("base do retangulo: ");
+  scanf ("%lf", &base);
+
+  printf ("altura do retangulo: ");
+  scanf ("%lf", &altura);
+
+
+    return 0;
+}
